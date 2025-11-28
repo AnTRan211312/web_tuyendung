fadf2076505ae8334388e197de7b9ba6ac03227f
An Tran <an01639419125@gmail.com>
2025-11-28 20:52:16 +0700

    first commit

--- BackEnd-Works/src/main/java/com/TranAn/BackEnd_Works/advice/exception/ResourceNotFoundException.java
+++ BackEnd-Works/src/main/java/com/TranAn/BackEnd_Works/advice/exception/ResourceNotFoundException.java
@@ -2,5 +2,6 @@ package com.TranAn.BackEnd_Works.advice.exception;
 
 public class ResourceNotFoundException extends RuntimeException {
     public ResourceNotFoundException(String s) {
+        super(s);
     }
 }
